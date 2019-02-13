c395a651d685862b16cc5408033293514cc0ebbd
vic <victordjohnson@gmail.com>
2019-02-13 08:46:37 -0600

    converted the numbers file and the sorting/searching file into commonJS modules so that I can run this through Node

--- sortingAndSearching.js
+++ sortingAndSearching.js
@@ -1,20 +1,45 @@
-// Bogo Sort
-const shuffle = arr => {
-    const swapIndex = arr.length;
-    let temp, randomIndex;
+var numbers = require("./numbers");
 
-    while (swapIndex !== 0) {
+// Randomly shuffle the elements of the array that's passed in.
+function shuffle(arr) {
+  let swapIndex = arr.length;
+  let temp, randomIndex;
 
-        randomIndex = Math.floor(Math.random() * swapIndex);
+  while (swapIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * swapIndex);
 
-        swapIndex -= 1;
+    swapIndex -= 1;
 
-        temp = arr[swapIndex];
-        arr[swapIndex] = arr[randomIndex];
-        arr[randomIndex] = temp; 
-    }
+    temp = arr[swapIndex];
+    arr[swapIndex] = arr[randomIndex];
+    arr[randomIndex] = temp;
+  }
 
-    return arr;
+  return arr;
 }
 
-const isSorted = arr => arr.every((v,i,a) => !i || a[i-1] <= v);
+// Determine if an array is properly sorted
+function isSorted(arr) {
+  return arr.every((v, i, a) => !i || a[i - 1] <= v);
+}
+
+// Shuffles array until it's sorted.
+function bogoSort(array) {
+  let iterations = 1;
+  shuffle(array);
+  while (isSorted(array) == false) {
+    array = shuffle(array);
+    console.log(iterations++, array);
+  }
+  return array;
+}
+
+// Bogo Sort
+module.exports = {
+  shuffle: shuffle,
+  isSorted: isSorted,
+  bogoSort: bogoSort
+};
+
+const testValues = numbers.eightNumbers;
+var sorted = bogoSort(testValues);
